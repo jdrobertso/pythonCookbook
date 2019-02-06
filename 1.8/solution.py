d10c1251692d0251e4e8e1336eb41864cf1aafbc
Jeremy Robertson <jrobertson.88@gmail.com>
2019-02-05 20:12:20 -0600

    Added 1.8

--- 1.8/solution.py
+++ 1.8/solution.py
@@ -5,3 +5,10 @@ prices = {
     'HPQ': 37.20,
     'FB': 10.75
 }
+
+min_price = min(zip(prices.values(), prices.keys()))
+
+max_price = max(zip(prices.values(), prices.keys()))
+
+print(min_price)
+print(max_price)
